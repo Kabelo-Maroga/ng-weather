6ec10c5b5326a84d7ece654c640138ed433949c7
QXZ3ADQ <kabelo.maroga@bmwithub.co.za>
2024-08-05 00:22:52 +0200

    finally removed weather.service.ts in location.service.ts

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from '@angular/core';
-import {WeatherService} from "./weather.service";
+import {Injectable} from '@angular/core';
 import {LocationFacade} from './location-state/location.facade';
 
 export const LOCATIONS : string = "locations";
@@ -9,13 +8,12 @@ export class LocationService {
 
   locations : string[] = [];
 
-  constructor(private weatherService : WeatherService, private locationFacade: LocationFacade ) {
+  constructor(private locationFacade: LocationFacade ) {
     let locString = localStorage.getItem(LOCATIONS);
     if (locString)
       this.locations = JSON.parse(locString);
     for (let loc of this.locations)
       this.locationFacade.addCurrentConditions(loc);
-      // this.weatherService.addCurrentConditions(loc);
   }
 
   addLocation(zipcode : string) {
@@ -30,7 +28,6 @@ export class LocationService {
       this.locations.splice(index, 1);
       localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
       this.locationFacade.removeCurrentConditions(zipcode);
-      // this.weatherService.removeCurrentConditions(zipcode);
     }
   }
 }
