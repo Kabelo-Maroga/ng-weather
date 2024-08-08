5118b4b623cddb12a789a75d08daaafaff8353a5
QXZ3ADQ <kabelo.maroga@bmwithub.co.za>
2024-08-08 11:03:36 +0200

    fixing defects.

--- src/app/components/current-conditions/current-conditions.component.ts
+++ src/app/components/current-conditions/current-conditions.component.ts
@@ -1,16 +1,18 @@
-import {AfterViewInit, Component, inject, OnInit, QueryList, TemplateRef, ViewChildren} from '@angular/core';
+import {AfterContentChecked, AfterViewInit, Component, inject, OnInit, QueryList, TemplateRef, ViewChildren} from '@angular/core';
 import {Router} from '@angular/router';
 import {WeatherFacade} from '../../core/store/weather/weather.facade';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {LOCATIONS} from '../../core/models/constants/cache.type';
 import {Tab} from '../../core/models/tab.type';
+import {distinctUntilChanged, tap} from 'rxjs/operators';
+import {ConditionsAndZip} from '../../core/models/conditions-and-zip.type';
 
 @Component({
   selector: 'app-current-conditions',
   templateUrl: './current-conditions.component.html',
   styleUrls: ['./current-conditions.component.css']
 })
-export class CurrentConditionsComponent implements OnInit, AfterViewInit {
+export class CurrentConditionsComponent implements OnInit, AfterViewInit, AfterContentChecked {
   protected router = inject(Router);
   protected weatherFacade = inject(WeatherFacade);
   protected currentConditionsByZip = toSignal(this.weatherFacade.currentConditions$);
@@ -36,15 +38,27 @@ export class CurrentConditionsComponent implements OnInit, AfterViewInit {
     this.createTemplates();
   }
 
+  ngAfterContentChecked(): void {
+    if(this.locationTemplates) {
+      this.createTemplates();
+    }
+  }
+
   showForecast(zipcode : string){
     this.router.navigate(['/forecast', zipcode])
+  }
+
+  tabExists(location: ConditionsAndZip): boolean {
+    return this.tabs.some(tab => tab.id === location.zip);
   }
 
   createTemplates() {
     const conditions = this.currentConditionsByZip();
     this.locationTemplates.forEach((templateRef, index) => {
       const location = conditions[index];
-      if (location && location.data && location.data.name) {
+      // const tabExists = this.tabs.some(tab => tab.id === location.zip);
+
+      if (location && location.data && location.data.name && !this.tabExists(location)) {
         this.tabs.push({
           id: location.zip,
           title: `${location.data.name} (${location.zip})`,
