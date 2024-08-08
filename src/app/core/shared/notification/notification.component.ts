024b3545edc0bb9ad517768b9c482da534d27f0c
QXZ3ADQ <kabelo.maroga@bmwithub.co.za>
2024-08-08 14:20:52 +0200

    created notification state; created notification component.

---
+++ src/app/core/shared/notification/notification.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {AsyncPipe, NgIf} from '@angular/common';
+import {NotificationFacade} from '../../store/notification/notification.facade';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [
+    NgIf,
+    AsyncPipe
+  ],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.css'
+})
+export class NotificationComponent {
+  @Input() message: string = '';
+  isVisible = true;
+
+  massage$ = this.notificationFacade.selectNotification$;
+
+  constructor(private notificationFacade: NotificationFacade) {
+  }
+  close() {
+    this.notificationFacade.dismissNotification(null);
+  }
+}
